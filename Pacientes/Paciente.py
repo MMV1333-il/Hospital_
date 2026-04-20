f96931f678b3df444870405a9c9368397bc8b6c0
Ilce Monserrat Montoya <ilce.montoyas@uanl.edu.mx>
2026-04-19 19:29:04 -0600

    Se creo la funcnion init con 5 atributos privados

--- Pacientes/Paciente.py
+++ Pacientes/Paciente.py
@@ -1,1 +1,10 @@
 #Creacion de carpeta y archivo, la clase hablara sobre pacientes del hospital
+class Paciente:
+    def __init__(self,nombre,edad,num_expediente,alergias,hist_cons):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__num_expediente = num_expediente
+        self.__alergias = alergias
+        self.__hist_cos = hist_cons
+
+    
