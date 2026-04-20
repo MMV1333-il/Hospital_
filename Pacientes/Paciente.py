a8cc21b4a73961e2fceae70beecd781290c68ddd
Ilce Monserrat Montoya <ilce.montoyas@uanl.edu.mx>
2026-04-19 21:05:00 -0600

    se movio algo (no se que)

--- Pacientes/Paciente.py
+++ Pacientes/Paciente.py
@@ -1,6 +1,6 @@
 #Creacion de carpeta y archivo, la clase hablara sobre pacientes del hospital
 class Paciente:
-    def __init__(self,nombre,edad,num_expediente,alergias = [], cant_cons = []):
+    def __init__(self,nombre,edad,num_expediente,alergias, cant_cons = []):
         self.__nombre = nombre
         self.__edad = edad
         self.__num_expediente = num_expediente
