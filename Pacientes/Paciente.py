edae63c4ce8ebf2cecc42ed7a236802974dc9143
Ilce Monserrat Montoya <ilce.montoyas@uanl.edu.mx>
2026-04-20 20:48:09 -0600

    Se corrigieron errores en el codigo, se modifico el procedimiento de el metodo de registro de consultas, y se probo el metodo info y el metodo registro de consultas

--- Pacientes/Paciente.py
+++ Pacientes/Paciente.py
@@ -1,6 +1,6 @@
 #Creacion de carpeta y archivo, la clase hablara sobre pacientes del hospital
 class Paciente:
-    def __init__(self,nombre,edad,num_expediente,alergias, cant_cons = []):
+    def __init__(self,nombre,edad,num_expediente,alergias = [],cant_cons = 0):
         self.__nombre = nombre
         self.__edad = edad
         self.__num_expediente = num_expediente
@@ -35,13 +35,20 @@ class Paciente:
     def info(self):
         print(f"numero de expediente del paciente: {self.__num_expediente}-nombre del paciente: {self.__nombre}-edad del paciente: {self.edad}")
 
-    def prog_cons(self,cant_cons):
-        print("Se ha programado una nueva consulta")
-        x = 0
-        for x in cant_cons:
-            x += 1
-        print(f"cantidad de consultas: {x}")
-        
+    def registro_cons(self):
+        self.__cant_cons += 1
+        print("Se creo una nueva consulta")
+        print(f"La cantidad de consultas es: {self.__cant_cons}")
     
+# CREANDO UN OBJETO PACIENTE PARA PROBAR LA CLASE Y SUS METODOS
+#SE CREO UN OBJETO
+p1 = Paciente("Karla",18,2345,["Fenozoparadina","Pregabalina","Amoxicilina"])
+#SE PRUEBA EL METODO INFO
+p1.info()
+#SE PRUEBA EL METODO REGISTRO DE CONSULTA
+p1.registro_cons()
+p1.registro_cons()
+
+
 
         
